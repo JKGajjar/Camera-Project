300bddfdf7e49c2c1e6ceec067a4fc530c2d445a
JKGajjar <14it030@charusat.edu.in>
2017-09-12 11:59:20 -0500

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,7 +11,6 @@ namespace WebCamSample
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private MediaCapture mediaCapture;
